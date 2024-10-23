1dfe9c6dd8943aed7fcdcc2bec3fde0fde2e49e0
SalsettiA <salsettia@hotmail.com>
2024-10-23 18:33:29 +0200

    Use argument on the command line to define the number of grids. Default: 1.

--- src/sudoku2.cpp
+++ src/sudoku2.cpp
@@ -9,12 +9,21 @@
 #include <iostream>
 #include "SudoAlgo.h"
 
-
-int main() {
-	cout << "main - info - begin" << endl;
-	SudoAlgo sudo_algo(1); // generate 1 solution and then stop
+int main(int argc, char **argv) {
+	cerr << "main - debug - begin" << endl;
+	unsigned int i = 1;
+	if ( argc > 1 )
+		try {
+		i = stoi(argv[1]);
+		}
+		catch (...) {
+			cout << "main - err - You can pass the number of grids per parameter. Default: 1." << endl <<
+					"main - err - Example: " << argv[0] << " 2." << endl <<
+					"main - err - Passing 0 as argument will print out all the solutions." << endl;
+		}
+	SudoAlgo sudo_algo(i); // generate 1 solution and then stop
 	sudo_algo.generate();
 //	sudo_algo.display();
-	cout << "main - info - end" << endl;
+	cerr << "main - debug - end" << endl;
 	return 0;
 }
